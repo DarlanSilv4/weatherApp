fa28aa037c24d83cc926213c0a70c6ba891c93ad
Darlan <Darlan.silvadacruz@gmail.com>
2021-03-27 11:00:40 -0300

    style: muda direitorio do api-service.js

--- js/service/api-service.js
+++ js/service/api-service.js
@@ -1,4 +1,4 @@
-import { API_KEY } from './config.js';
+import { API_KEY } from '../config.js';
 
 const searchCity = (cityName) => {
     return fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${cityName}`)
