be90e57f510cf2eace72b344c4d63dcc3153ab8f
Darlan <Darlan.silvadacruz@gmail.com>
2021-03-27 09:56:25 -0300

    feat: configura busca de cidades na api

---
+++ js/api-service.js
@@ -0,0 +1,12 @@
+import { API_KEY } from './config.js';
+
+const searchCity = (cityName) => {
+    return fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${cityName}`)
+        .then(response => {
+            return response.json();
+        });
+}
+
+export const apiService = {
+    searchCity
+}
