ef41c77cc135b2501e65f69fc2454b19509797d6
Darlan <Darlan.silvadacruz@gmail.com>
2021-04-02 16:15:11 -0300

    feat: salva no localstorage o nome da ultima cidade buscada

--- js/controller/getCity-controller.js
+++ js/controller/getCity-controller.js
@@ -2,5 +2,6 @@ import { apiService } from '../service/api-service.js';
 
 export const getCity = async (cityName) => {
     const city = await apiService.searchCity(cityName);
+    localStorage.setItem('city', city[0].LocalizedName);
     return city[0];
 }
