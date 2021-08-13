45f637671e1a564dc4677b9338aaa14b3a04ca0b
Darlan <darlan.silvadacruz@gmail.com>
2021-08-13 09:59:38 -0300

    refactor(api-service.js): simplifica o codigo das funcoes assincronas
    Nas funcoes assincronas passou-se a utilizar await ao inves de then para a simplificacao do codigo

--- js/service/api-service.js
+++ js/service/api-service.js
@@ -1,17 +1,13 @@
 import { API_KEY } from '../config.js';
 
-const searchCity = (cityName) => {
-    return fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${cityName}`)
-        .then(response => {
-            return response.json();
-        });
+const searchCity = async (cityName) => {
+    const response = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${API_KEY}&q=${cityName}`);
+    return response.json();
 }
 
-const getWeather = (cityKey) => {
-    return fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${API_KEY}`)
-        .then(response => {
-            return response.json();
-        });
+const getWeather = async (cityKey) => {
+    const response = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${API_KEY}`);
+    return response.json();
 }
 
 export const apiService = {
