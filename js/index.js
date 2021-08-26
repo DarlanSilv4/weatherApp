2010774e3ef6607377f5782a9c88d6e71554f704
Darlan <darlan.silvadacruz@gmail.com>
2021-08-26 09:45:36 -0300

    refactor(index.js): diminue responsabilidades da funcao search
    diminue responsabilidades da funcao search, separando em outras funcoes para melhorar a leitura e a
    manutencao do codigo

--- js/index.js
+++ js/index.js
@@ -2,32 +2,48 @@ import { createForecastCard } from './components/forecast-card.js';
 import { apiService } from './service/api-service.js';
 import { setHeader } from './components/header.js'
 import { util } from './util.js';
+import { createWeatherCard } from './components/weather-card.js';
 
 const searchBar = document.getElementById('search-city');
 searchBar.addEventListener("keyup", (event) => {
-    if (event.key == "Enter") {
-        const cityName = searchBar.value;
-        search(cityName);
-        searchBar.value = '';
-    }
+    if (event.key !== "Enter") return;
+
+    const cityName = searchBar.value;
+    search(cityName);
+    searchBar.value = '';
 });
 
 const search = async (cityName) => {
     const city = await apiService.getCity(cityName);
-    localStorage.setItem('city', city.LocalizedName); //save city name in localStorage
+    if (city === null) return showErrorMessage();
 
-    if (city == null) {
-        showErrorMessage();
-        return;
-    }
+    saveLastCityNameInLocalStorage(city.LocalizedName);
+    return setupPage(city);
+}
+
+const setupPage = async (city) => {
     setHeader(city);
 
+    const weather = await apiService.getCurrentWeather(city.Key);
+    if (weather === null) return showErrorMessage();
+    setWeather(weather[0]);
+
     const forecast = await apiService.getDailyForecast(city.Key);
-    if (forecast == null) {
-        showErrorMessage();
-        return;
-    }
+    if (forecast === null) return showErrorMessage();
     setForecast(forecast);
+}
+
+const saveLastCityNameInLocalStorage = cityName => {
+    return localStorage.setItem('city', cityName);
+}
+
+const setWeather = (weather) => {
+    const weatherCard = document.getElementById('weather-card');
+    if (weatherCard) weatherCard.remove();
+
+    const card = createWeatherCard(weather);
+    const cityInfo = document.getElementById('city-info');
+    cityInfo.after(card);
 }
 
 const setForecast = (forecast) => {
@@ -44,11 +60,9 @@ const showErrorMessage = () => {
     alert("😭 Sorry, we could not find the city you are looking for...");
 }
 
-const load = () => {
+const autoSearch = () => {
     const cityName = localStorage.getItem('city') || false;
-    if (cityName) {
-        search(cityName);
-    }
+    if (cityName) search(cityName);
 }
 
-load();
+autoSearch();
